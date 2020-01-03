7f38e5f3380ffd973c05bc3c93d85cb2a21befa4
Sri Sanka <snsak47@gmail.com>
2020-01-03 00:23:06 -0500

    Data files seperation

--- fitness-web/src/Components/HomePageCarousel.js
+++ fitness-web/src/Components/HomePageCarousel.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { Gallery, GalleryImage } from "react-gesture-gallery";
-const images = [
-  "https://images.unsplash.com/photo-1517344687790-7338f238f7f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80",
-  "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1335&q=80",
-  "https://images.unsplash.com/photo-1537907235743-0617671463d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-  "https://morehouseathletics.com/images/2019/9/7/Morehouse_EWC_Score_Graphic.jpg?width=1920&quality=80&format=jpg",
-  "https://images.unsplash.com/photo-1540496905036-5937c10647cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-];
+import { images } from "../Data/Utilities";
 
 function HomePageCarousel() {
   const [index, setIndex] = React.useState(0);
